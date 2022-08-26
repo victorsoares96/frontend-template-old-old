b74e6a9e03f833a81dedad95b450bfd4e2bcf20b
Victor Soares <vitorsoares96@hotmail.com>
2022-08-26 16:49:16 -0300

    first commit

---
+++ src/config/index.ts
@@ -0,0 +1,67 @@
+import isMobile from '@/utils/is-mobile';
+
+import manifestJSON from '../../manifest.json';
+import packageJSON from '../../package.json';
+import type { Notifications } from './types';
+
+const title = manifestJSON.name;
+
+const email = packageJSON.bugs.email;
+
+const repository = packageJSON.repository.url;
+
+const messages = {
+  app: {
+    crash: {
+      title: 'Oooops... Sorry, I guess, something went wrong. You can:',
+      options: {
+        email: `contact with author by this email - ${email}`,
+        reset: 'Press here to reset the application',
+      },
+    },
+  },
+  loader: {
+    fail: 'Hmmmmm, there is something wrong with this component loading process... Maybe trying later would be the best idea',
+  },
+  images: {
+    failed: 'something went wrong during image loading :(',
+  },
+  404: 'Hey bro? What are you looking for?',
+};
+
+const dateFormat = 'MMMM DD, YYYY';
+
+const notifications: Notifications = {
+  options: {
+    anchorOrigin: {
+      vertical: 'bottom',
+      horizontal: 'left',
+    },
+    autoHideDuration: 6000,
+  },
+  maxSnack: isMobile ? 3 : 4,
+};
+
+const loader = {
+  // no more blinking in your app
+  delay: 300, // if your asynchronous process is finished during 300 milliseconds you will not see the loader at all
+  minimumLoading: 700, // but if it appears, it will stay for at least 700 milliseconds
+};
+
+const defaultMetaTags = {
+  image: '/cover.png',
+  description: packageJSON.description,
+};
+const giphy404 = 'https://giphy.com/embed/xTiN0L7EW5trfOvEk0';
+
+export {
+  loader,
+  notifications,
+  dateFormat,
+  messages,
+  repository,
+  email,
+  title,
+  defaultMetaTags,
+  giphy404,
+};
